49d2bf480ad76c72cecd11a1e7375e44567302b5
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-05-09 10:50:47 -0500

    changed title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,56 +1,63 @@
-import Link from 'next/link';
-import { FaShoppingCart } from 'react-icons/fa';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useSnipcart } from "use-snipcart";
+import { FaShoppingCart } from "react-icons/fa";
 
-import Container from '@components/Container';
+import Container from "@components/Container";
 
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
 
 const Header = () => {
+  const { locale: activeLocale, locales, asPath } = useRouter();
+  const { cart = {} } = useSnipcart();
+
+  const availableLocales = locales.filter((locale) => locale !== activeLocale);
+
   return (
     <header className={styles.header}>
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            <a>Space Jelly</a>
+            <a>Fran_The_Dev</a>
           </Link>
         </p>
         <ul className={styles.headerLinks}>
           <li>
-            <Link href="#">
-              <a>Link</a>
+            <Link href="/categories/apparel">
+              <a>Apparel</a>
             </Link>
           </li>
           <li>
-            <Link href="#">
-              <a>Link</a>
+            <Link href="/categories/accessories">
+              <a>Accessories</a>
             </Link>
           </li>
           <li>
-            <Link href="#">
-              <a>Link</a>
+            <Link href="/stores">
+              <a>Find a Store</a>
             </Link>
           </li>
         </ul>
         <p className={styles.headerCart}>
-          <button>
+          <button className="snipcart-checkout">
             <FaShoppingCart />
-            <span>
-              $0.00
-            </span>
+            <span>${cart.subtotal?.toFixed(2)}</span>
           </button>
         </p>
         <ul className={styles.headerLocales}>
-          <li>
-            <Link href="#">
-              <a>
-                ES
-              </a>
-            </Link>
-          </li>
+          {availableLocales.map((locale) => {
+            return (
+              <li key={locale}>
+                <Link href={asPath} locale={locale}>
+                  <a>{locale.toUpperCase()}</a>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </Container>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,43 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import Button from '@components/Button';
+import { buildImage } from "@lib/cloudinary";
 
-import products from '@data/products';
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import Button from "@components/Button";
 
-import styles from '@styles/Page.module.scss'
+import products from "@data/products";
 
-export default function Home() {
+import styles from "@styles/Page.module.scss";
+
+export default function Home({ home, products }) {
+  const { heroTitle, heroText, heroLink, heroBackground } = home;
   return (
     <Layout>
       <Head>
-        <title>Space Jelly Gear</title>
+        <title>Fran_The_Dev</title>
         <meta name="description" content="Get your Space Jelly gear!" />
       </Head>
 
       <Container>
-        <h1 className="sr-only">Space Jelly Gear</h1>
+        <h1 className="sr-only">Fran_The_Dev - Space Jelly Gear</h1>
 
         <div className={styles.hero}>
-          <Link href="#">
+          <Link href={heroLink}>
             <a>
               <div className={styles.heroContent}>
-                <h2>Prepare for liftoff.</h2>
-                <p>Apparel that&apos;s out of this world!</p>
+                <h2>{heroTitle}</h2>
+                <p>{heroText}</p>
               </div>
-              <img className={styles.heroImage} src="/images/space-jelly-gear-banner.jpg" alt="" />
+              <img
+                className={styles.heroImage}
+                width={heroBackground.width}
+                height={heroBackground.height}
+                src={buildImage(heroBackground.public_id).toURL()}
+                alt=""
+              />
             </a>
           </Link>
         </div>
@@ -35,32 +45,99 @@ export default function Home() {
         <h2 className={styles.heading}>Featured Gear</h2>
 
         <ul className={styles.products}>
-          {products.slice(0, 4).map(product => {
+          {products.map((product) => {
+            const imageUrl = buildImage(product.image.public_id)
+              .resize("w_900,h_900")
+              .toURL();
             return (
-              <li key={product.id}>
-                <Link href="#">
+              <li key={product.slug}>
+                <Link href={`/products/${product.slug}`}>
                   <a>
                     <div className={styles.productImage}>
-                      <img width="500" height="500" src={product.image} alt="" />
+                      <img
+                        width={product.image.width}
+                        height={product.image.height}
+                        src={imageUrl}
+                        alt=""
+                      />
                     </div>
-                    <h3 className={styles.productTitle}>
-                      { product.name }
-                    </h3>
-                    <p className={styles.productPrice}>
-                      ${ product.price }
-                    </p>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
                   </a>
                 </Link>
                 <p>
-                  <Button>
+                  <Button
+                    className="snipcart-add-item"
+                    data-item-id={product.id}
+                    data-item-price={product.price}
+                    data-item-url={`/products/${product.slug}`}
+                    data-item-image={product.image.url}
+                    data-item-name={product.name}
+                  >
                     Add to Cart
                   </Button>
                 </p>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps({ locale }) {
+  const client = new ApolloClient({
+    uri: "https://api-us-east-1.graphcms.com/v2/ckzvrda212z1d01za7m8y55rc/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageHome($locale: Locale!) {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          slug
+          heroBackground
+          localizations(locales: [$locale]) {
+            heroText
+            heroTitle
+            locale
+          }
+        }
+        products(where: { categories_some: { slug: "featured" } }) {
+          id
+          name
+          price
+          slug
+          image
+        }
+      }
+    `,
+    variables: {
+      locale,
+    },
+  });
+
+  let home = data.data.page;
+
+  if (home.localizations.length > 0) {
+    home = {
+      ...home,
+      ...home.localizations[0],
+    };
+  }
+
+  const products = data.data.products;
+
+  return {
+    props: {
+      home,
+      products,
+    },
+  };
 }
